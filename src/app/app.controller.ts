feda76caa61c431af84ed36161cdd292c2e572c5
Ruslan <glpat-E72suTk57q-yeoxm-Tyn>
2025-04-23 10:56:11 +0300

    connect server

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -9,4 +9,13 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get()
+  connect(): { status: string; data: null; message: string } {
+    return {
+      status: 'success',
+      data: null,
+      message: 'Соединение востановленно',
+    };
+  }
 }
